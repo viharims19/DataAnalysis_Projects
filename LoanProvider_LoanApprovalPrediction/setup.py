c21b116b58b5c2e46ef7a08c7f409235d3dcb460
SaiVihari-Mukkamala <sai.vihari95@gmail.com>
2024-07-21 20:02:25 +0530

    install requirements

---
+++ LoanProvider_LoanApprovalPrediction/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="loan_approval_predictor",
+    version="0.0.0",
+    author="vihari",
+    author_email="sai.vihari95@gmail.com",
+    packages=find_packages()
+)
